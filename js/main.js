53058b9d661c58f7f18008e6316524c20af2b7d9
Gabriel Walford <gabriel@gabrielwalford.com>
2015-05-12 12:45:36 -0600

    created circle pulse chart

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
   console.log('App Started');
 
-  var socket = io('http://brand.nioinstances.com');
+  var socket = io('http://brand.nioinstances.com'),
+      room = 'gabriel',
+      $window = $(window),
+      $circleChart = $('#circle-chart'),
+      $barChart = $('#bar-chart');
+
+  var bars = new BarChart();
+  var circles = new CircleChart();
 
   socket.on('connect', function() {
     console.log('Connected');
@@ -9,7 +16,42 @@ $(document).ready(function() {
   });
 
   socket.on('recvData', function(data) {
-    console.log(data);
+    var dataObj = JSON.parse(data);
+
+    circles.addNewCircle(dataObj.count);
+    bars.addNewBar(dataObj.count);
   });
+
+  function CircleChart() {
+    this.addNewCircle = function(radius) {
+      $circleChart.append('<div class="circle"></div>');
+      $circleChart.children().last().animate({
+        width: radius*2 + 'px', 
+        height: radius*2 + 'px', 
+        'margin-top': -radius + 'px',
+        'margin-left': -radius + 'px'
+      }, 1000);
+
+      this.removeOldCircles();
+    };
+
+    this.removeOldCircles = function() {
+      var circles = $circleChart.children();
+      if ( circles.length > 2 ) {
+        circles.first().animate({opacity: 0}, 1000, function(){
+           this.remove();
+        });
+      }
+    };
+  };
+
+  function BarChart() {
+    this.addNewBar = function(height) {
+      console.log('Add New Circle = ', height);
+    };
+
+    this.removeOldBar = function() {
+    };
+  };
   
 });
